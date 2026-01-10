bc784085acac1aa4be9c3171c24c0e4fb4b37273
Obed <231944182+obedobedobed@users.noreply.github.com>
2026-01-10 14:11:52 +0300

    trees generating added
       @@@ 
    @@@@@
    @@#@@
           #
           #

--- KomaruWorld/Game/GameWorld/WorldGenerator.cs
+++ KomaruWorld/Game/GameWorld/WorldGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using static KomaruWorld.GameParameters;
 
@@ -5,8 +6,21 @@ namespace KomaruWorld;
 
 public static class WorldGenerator
 {
+    private static string[] tree =
+    {
+        " @@@ ",
+        "@@@@@",
+        "@@#@@",
+        "  #  ",
+        "  #  "
+    };
+    private static int treeSpawnChance = 30;
+
     public static void Generate(int width, int height)
     {
+        bool canGenerateTrees = true;
+        int generatedTreeTilesAgo = 0;
+
         float yPos = 0;
         for (int y = 0; y < height; y++)
         {
@@ -24,6 +38,44 @@ public static class WorldGenerator
                     targetTile = TilesBank.Grass(targetPosition);
                 else
                     targetTile = null;
+
+                if (y == 15 - tree.Length && Random.Shared.Next(0, 101) <= treeSpawnChance && canGenerateTrees)
+                {
+                    GenerateTree(xPos, yPos);
+                    generatedTreeTilesAgo = 0;
+                    canGenerateTrees = false;
+                }
+
+                if (generatedTreeTilesAgo > tree[0].Length)
+                    canGenerateTrees = true;
+
+                generatedTreeTilesAgo++;
+
+                if (targetTile != null)
+                    World.AddTile(targetTile);
+
+                xPos += TileSize.X;
+            }
+
+            yPos += TileSize.Y;
+        }
+    }
+
+    public static void GenerateTree(float baseX, float baseY)
+    {
+        float yPos = baseY;
+        for (int y = 0; y < tree.Length; y++)
+        {
+            float xPos = baseX;
+            for (int x = 0; x < tree[0].Length; x++)
+            {
+                Vector2 targetPosition = new Vector2(xPos, yPos);
+                Tile targetTile = tree[y][x] switch
+                {
+                    '@' => TilesBank.Leaf(targetPosition),
+                    '#' => TilesBank.Log(targetPosition),
+                    _ => null,
+                };
 
                 if (targetTile != null)
                     World.AddTile(targetTile);
