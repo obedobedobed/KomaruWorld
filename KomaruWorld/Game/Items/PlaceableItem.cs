4fa1ee65ccfc0fde946c7d13f775f8ebacf73ef0
Obed <231944182+obedobedobed@users.noreply.github.com>
2026-01-18 11:05:40 +0300

    Delete KomaruWorld/Game/Items/PlaceableItem.cs bugfix

--- KomaruWorld/Game/Items/PlaceableItem.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Xna.Framework.Graphics;
-
-namespace KomaruWorld;
-
-public class PlaceableItem(string name, int maxStack, Texture2D texture, int id, Tiles placeTile)
-: Item(name, maxStack, texture, id)
-{
-    public Tiles ItemTile { get; private set; } = placeTile;
-}
